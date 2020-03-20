c82174a5998973818a6e437d239f7107a13d62a7
raph941 <raph941@gmail.com>
2020-03-20 15:37:47 +0100

    added a welcome message for base url

--- users/api/views.py
+++ users/api/views.py
@@ -1,16 +1,23 @@
 # users/views.py
 from rest_framework import generics
 from users.models import CustomUser
+from django.http import HttpResponse
 
 from users.models import CustomUser, CustomUserProfile
 from .serializers import UserSerializer
 
+
+def home(request):
+    return HttpResponse("welcome to skimake print api. append '/swagger' to the base url to view documentation")
+
+
 class UserListView(generics.ListAPIView):
-    queryset = CustomUser.objects.all()
-    serializer_class = UserSerializer
+    queryset                = CustomUser.objects.all()
+    serializer_class        = UserSerializer
+
 
 class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
-    lookup_field = 'pk'
-    queryset = CustomUser.objects.all()
-    serializer_class = UserSerializer
+    lookup_field            = 'pk'
+    queryset                = CustomUser.objects.all()
+    serializer_class        = UserSerializer
     
