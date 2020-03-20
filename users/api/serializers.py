043348ce5ab2e4378d1d41b7382659648029c459
raph941 <raph941@gmail.com>
2020-03-20 10:25:41 +0100

    model update anda api modifications to display nested relationships (profile and wallet) on user object

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -1,14 +1,24 @@
 # users/serializers.py
-from rest_framework import serializers
-from users.models import CustomUser, CustomUserProfile
+from rest_framework import serializers 
+from users.models import CustomUser, CustomUserProfile, CustomUserWallet
+from drf_writable_nested.serializers import WritableNestedModelSerializer
+from drf_writable_nested.mixins import  UniqueFieldsMixin, NestedUpdateMixin
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserProfileSerializer(UniqueFieldsMixin, NestedUpdateMixin, serializers.ModelSerializer):
+    class Meta:
+        model               = CustomUserProfile
+        fields              = '__all__'
+
+class UserWalletSerializer(UniqueFieldsMixin, NestedUpdateMixin, serializers.ModelSerializer):
+    class Meta:
+        model               = CustomUserWallet
+        fields              = '__all__'
+
+class UserSerializer(UniqueFieldsMixin, NestedUpdateMixin, serializers.ModelSerializer):
+    customuserprofile       = UserProfileSerializer()
+    customuserwallet        = UserWalletSerializer()
     class Meta:
         model               = CustomUser
         fields              = ('__all__')
 
-class UserProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model               = CustomUserProfile
-        fields              = '__all__'
